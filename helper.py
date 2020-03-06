3cad955c15a9278f363ed3cf1d964b4ecf6e63f2
VoidlessSeven7 <37181533+VoidlessSeven7@users.noreply.github.com>
2020-03-06 12:49:34 +0100

    Organized helper script

--- helper.py
+++ helper.py
@@ -2,11 +2,7 @@ from config import debug
 from json import load as load_json
 
 
-# Prints if debug is true
-def debug_print(text):
-    if debug:
-        print('\n %s' % (text))
-
+# -------- Init -------- #
 
 # Hardcoded max_count, would probably need to use a fetch
 with open('assets/lib/rule-34-shared-resources/domains.json', 'r') as json_data:
@@ -15,3 +11,13 @@ with open('assets/lib/rule-34-shared-resources/domains.json', 'r') as json_data:
     # Delete domains that we are not gonna use
     list_of_domains[:] = [
         d for d in list_of_domains if d.get('short') != 'loli']
+
+
+# -------- Helper functions -------- #
+
+# Prints if debug is true
+def debug_print(text):
+    if debug:
+        print('\n %s' % (text))
+
+
